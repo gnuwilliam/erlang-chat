5fe86840d98995861cd9ed2c3b025a4055b7174b
William Oliveira <sudowilliam@gmail.com>
2014-02-20 15:36:22 -0300

    print_message function to handle received messages

--- chat_client.erl
+++ chat_client.erl
@@ -4,3 +4,7 @@
 
 send_message(RouterPid, Addressee, MessageBody) ->
   message_router:send_chat_message(RouterPid, Addressee, MessageBody).
+
+print_message(MessageBody) ->
+  io:format("Received: ~p~n", [MessageBody]).
+
