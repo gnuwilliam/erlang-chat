7c5ca480840a568dfc75980d92098e8fb463be76
William Oliveira <sudowilliam@gmail.com>
2014-02-20 15:36:29 -0300

    refactor message_router

--- message_router.erl
+++ message_router.erl
@@ -2,7 +2,7 @@
 
 -compile(export_all).
 
-start() ->
+start(PrintFunction) ->
   spawn(message_router, route_messages, []).
 
 stop(RouterPid) ->
@@ -11,16 +11,16 @@ stop(RouterPid) ->
 send_chat_message(RouterPid, Addressee, MessageBody) ->
   RouterPid ! { send_chat_msg, Addressee, MessageBody }.
 
-route_messages() ->
+route_messages(PrintFunction) ->
   receive
     { send_chat_msg, Addressee, MessageBody } ->
       Addressee ! { receive_chat_msg, MessageBody },
-      route_messages();
+      route_messages(PrintFunction);
     { receive_chat_msg, MessageBody } ->
-      io:format("Received ~p~n", [MessageBody]);
+      PrintFunction(MessageBody);
     shutdown ->
       io:format("Shutting down~n");
     Oops ->
       io:format("Warning! Received ~p~n", [Oops]),
-      route_messages()
+      route_messages(PrintFunction)
   end.
