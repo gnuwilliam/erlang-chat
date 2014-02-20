e8604b432b04455427fd33ac683b6fc8d0741f8a
William Oliveira <sudowilliam@gmail.com>
2014-02-20 15:51:35 -0300

    start_router function passing the function reference

--- chat_client.erl
+++ chat_client.erl
@@ -8,3 +8,5 @@ send_message(RouterPid, Addressee, MessageBody) ->
 print_message(MessageBody) ->
   io:format("Received: ~p~n", [MessageBody]).
 
+start_router() ->
+  message_router:start(fun chat_client:print_message/1).
