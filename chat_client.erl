41467faacc2a4e10c839d2a5efe07ba0ca55ec45
William Oliveira <sudowilliam@gmail.com>
2014-02-23 02:08:31 -0300

    refactoring

--- chat_client.erl
+++ chat_client.erl
@@ -2,11 +2,11 @@
 
 -compile(export_all).
 
-send_message(RouterPid, Addressee, MessageBody) ->
-  message_router:send_chat_message(RouterPid, Addressee, MessageBody).
+send_message(Addressee, MessageBody) ->
+    message_router:send_chat_message(Addressee, MessageBody).
 
 print_message(MessageBody) ->
-  io:format("Received: ~p~n", [MessageBody]).
+    io:format("Received: ~p~n", [MessageBody]).
 
 start_router() ->
-  message_router:start(fun chat_client:print_message/1).
+    message_router:start(fun chat_client:print_message/1).
--- message_router.erl
+++ message_router.erl
@@ -5,24 +5,26 @@
 -compile(export_all).
 
 start(PrintFunction) ->
-  spawn(message_router, route_messages, [PrintFunction]).
+    Pid = spawn(message_router, route_messages, [PrintFunction]),
+    erlang:register(?SERVER, Pid),
+    Pid.
 
-stop(RouterPid) ->
-  RouterPid ! shutdown.
+stop() ->
+    ?SERVER ! shutdown.
 
-send_chat_message(RouterPid, Addressee, MessageBody) ->
-  RouterPid ! { send_chat_msg, Addressee, MessageBody }.
+send_chat_message(Addressee, MessageBody) ->
+    ?SERVER ! { send_chat_msg, Addressee, MessageBody }.
 
 route_messages(PrintFunction) ->
-  receive
-    { send_chat_msg, Addressee, MessageBody } ->
-      Addressee ! { receive_chat_msg, MessageBody },
-      route_messages(PrintFunction);
-    { receive_chat_msg, MessageBody } ->
-      PrintFunction(MessageBody);
-    shutdown ->
-      io:format("Shutting down~n");
-    Oops ->
-      io:format("Warning! Received ~p~n", [Oops]),
-      route_messages(PrintFunction)
-  end.
+    receive
+        { send_chat_msg, Addressee, MessageBody } ->
+            Addressee ! { receive_chat_msg, MessageBody },
+            route_messages(PrintFunction);
+        { receive_chat_msg, MessageBody } ->
+            PrintFunction(MessageBody);
+        shutdown ->
+            io:format("Shutting down~n");
+        Oops ->
+            io:format("Warning! Received ~p~n", [Oops]),
+            route_messages(PrintFunction)
+    end.
