0dc69d23d91342db3069f0e37635e4786012b27b
William Oliveira <sudowilliam@gmail.com>
2014-02-24 01:18:45 -0300

    refactor

--- message_router.erl
+++ message_router.erl
@@ -14,8 +14,8 @@ stop() ->
 send_chat_message(Addressee, MessageBody) ->
     ?SERVER ! { send_chat_msg, Addressee, MessageBody }.
 
-register_nick(ClientName, PrintFun) ->
-    ?SERVER ! { register_nick, ClientName, PrintFun }.
+register_nick(ClientName, ClientPid) ->
+    ?SERVER ! { register_nick, ClientName, ClientPid }.
 
 unregister_nick(ClientName) ->
     ?SERVER ! { unregister_nick, ClientName }.
@@ -23,20 +23,24 @@ unregister_nick(ClientName) ->
 route_messages(Clients) ->
     receive
         { send_chat_msg, ClientName, MessageBody } ->
-            ?SERVER ! { recv_chat_msg, ClientName, MessageBody },
-            route_messages(Clients);
-        { recv_chat_msg, ClientName, MessageBody } ->
             case dict:find(ClientName, Clients) of
-                { ok, PrintFun } ->
-                    PrintFun(MessageBody);
+                { ok, ClientPid } ->
+                    ClientPid ! { pringmsg, MessageBody };
                 error ->
-                    io:format("Unknown client~n")
+                    io:format("Error! Unknown client: ~p~n", [ClientName])
             end,
             route_messages(Clients);
-        { register_nick, ClientName, PrintFun } ->
-            route_messages(dict:store(ClientName, PrintFun, Clients));
+        { register_nick, ClientName, ClientPid } ->
+            route_messages(dict:store(ClientName, ClientPid, Clients));
         { unregister_nick, ClientName } ->
-            route_messages(dict:erase(ClientName, Clients));
+            case dict:find(ClientName, Clients) of
+                { ok, ClientPid } ->
+                    ClientPid ! stop,
+                    route_messages(dict:erase(ClientName, Clients));
+                error ->
+                    io:format("Error! Unknown client: ~p~n", [ClientName]),
+                    route_messages(Clients)
+            end;
         shutdown ->
             io:format("Shutting down~n");
         Oops ->
